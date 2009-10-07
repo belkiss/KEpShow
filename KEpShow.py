6d1b0ff303a018c5f24f0458007241997c6468e9
Lambert CLARA <lambert.clara@yahoo.fr>
2009-10-07 19:58:41 +0200

    * Fill the screen with the tab * Read lastShows file and display according infos

--- KEpShow.py
+++ KEpShow.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# pyuic4 -o ui_KEpShow.py KEpShow.ui
 # File : KEpShow.py
 import datetime,locale,os,re,string,sys,time,urllib2
 from PyQt4 import QtCore, QtGui
@@ -6,6 +7,8 @@ from PyQt4 import QtCore, QtGui
 from ui_KEpShow import Ui_MainQWidget
 
 today = datetime.datetime.now().strftime("%d/%m/%Y")
+
+showSeenTo = {}
 
 def parsePage(view, page, dirpath):
 	locale.setlocale(locale.LC_ALL, 'en_US')
@@ -15,7 +18,7 @@ def parsePage(view, page, dirpath):
 	request.add_header('Cookie', 'ListDisplay=tvrage.com')
 	webpage = urllib2.urlopen(request).readlines()
 	found_first = 0
-	model = QtGui.QStandardItemModel(0, 2)
+	model = QtGui.QStandardItemModel(0, 3)
 
 	found = 0
 	dir_content = []
@@ -25,7 +28,7 @@ def parsePage(view, page, dirpath):
 		else:
 			for child_files in os.listdir(dirpath + "/" + files):
 				if os.path.isfile(os.path.join(str(dirpath + "/" + files), child_files)):
-					dir_content.append(string.lower(child_files))
+					dir_content.append(child_files)
 	
 	for line in webpage:
 		if found_first == 0:
@@ -35,15 +38,34 @@ def parsePage(view, page, dirpath):
 			split_line = re.search("\d*\s*(\d*)[-](\d*).{15}\s*(\d*)[/]([a-zA-Z]*)[/](\d*)",line)
 			if split_line:
 				diffusion_date = time.strptime("%02d" % int(split_line.group(3)) + "/" + split_line.group(4) + "/" + split_line.group(5),"%d/%b/%y")
-				season_nb  = "%02d" % int(split_line.group(1))
-				episode_nb = "%02d" % int(split_line.group(2))
-				str_cat    = "s" + season_nb +"e"+episode_nb
+				season_nb  = int(split_line.group(1))
+				episode_nb = int(split_line.group(2))
+				str_cat    = "s" + "%02d" % season_nb + "e" + "%02d" % episode_nb
 				found = False
+				fileName = ""
+
+				lowpage = string.lower(str(page))
+				#print "page " + lowpage + str(str(lowpage) in showSeenTo)
+				if lowpage in showSeenTo:
+					#print "page " + lowpage + showSeenTo[str(lowpage)]
+					split_ep = re.search("(\d*)[e](\d*)", showSeenTo[str(lowpage)])
+					if split_ep:
+						if season_nb < int(split_ep.group(1)):
+							found = True
+						elif season_nb == int(split_ep.group(1)):
+							if episode_nb <= int(split_ep.group(2)):
+								found = True
+
 				for mkv in dir_content:
-					if string.find(mkv, str_cat) != -1:
+					if string.find(string.lower(string.replace(mkv, "720p", "", 1)), str_cat) != -1:
 						dir_content.remove(mkv)
+						fileName = mkv
 						found = True
-						#print mkv + " " + str_cat + " " + str(string.find(mkv, str_cat))
+					elif string.find(string.lower(string.replace(mkv, "720p", "", 1)), str(season_nb) + str("%02d" % episode_nb)) != -1:
+						dir_content.remove(mkv)
+						fileName = mkv
+						found = True
+						#print mkv + " " + str(season_nb) + episode_nb + " " + str(string.find(string.lower(mkv), str(season_nb) + episode_nb))
 
 				diffusion_date = datetime.datetime(*diffusion_date[0:7]).strftime("%d/%m/%Y")
 				check = datetime.datetime.strptime(diffusion_date,"%d/%m/%Y") - datetime.datetime.strptime(today,"%d/%m/%Y")
@@ -53,10 +75,16 @@ def parsePage(view, page, dirpath):
 				addRoot(model, found)
 				addLine(model, 0, str_cat)
 				addLine(model, 1, diffusion_date, color)
+				if len(fileName)>0:
+					rar_extension = re.search("r(\d*)$",fileName)
+					if rar_extension:
+						addLine(model, 2, "RAR FILES " + fileName)
+					else:
+						addLine(model, 2, fileName)#color)
+
 
 	model.sort(0)
 	view.setModel(model)
-	
 
 class KEpShow(QtGui.QWidget):
 	def updateActions(self, rank):
@@ -95,6 +123,7 @@ def addRoot(model, name):
 	model.setData(model.index(0, 0), QtGui.QColor(colorCode), QtCore.Qt.BackgroundColorRole)
 	model.setData(model.index(0, 1), QtGui.QColor(colorCode), QtCore.Qt.BackgroundColorRole)
 	model.setData(model.index(0, 2), QtGui.QColor(colorCode), QtCore.Qt.BackgroundColorRole)
+	model.setData(model.index(0, 3), QtGui.QColor(colorCode), QtCore.Qt.BackgroundColorRole)
 	#model.setData(model.index(0, 0), QtCore.QVariant(name))
 
 def addLine(model, ix, name, colorCode = ""):
@@ -102,9 +131,25 @@ def addLine(model, ix, name, colorCode = ""):
 		model.setData(model.index(0, ix), QtGui.QColor(colorCode), QtCore.Qt.BackgroundColorRole)
 	model.setData(model.index(0, ix), QtCore.QVariant(name))
 
+def parseLastShows():
+	path = "/home/belkiss/download/Series"
+	lastShowsFilePath = os.path.join(path, "lastShows")
+	lastShowsFile = open(lastShowsFilePath, 'r')
+	stop = False
+	for line in lastShowsFile:
+		show_name  = line[0:string.find(line,' ')]
+		if string.find(show_name,'###############') != -1:
+			stop = True
+			#print "heeee truuuuue"
+		if stop == False:
+			split_line = re.search(".*[s](\d*)[e](\d*).*(\d*)[/](\d*)[/](\d*)",line)
+			if split_line:
+				#print ":" + string.lower(show_name) + ":"
+				showSeenTo[string.lower(show_name)] = split_line.group(1) + "e" + split_line.group(2)
 
 #if __name__ == "__main__":
 app = QtGui.QApplication(sys.argv)
+parseLastShows()
 kepshow = KEpShow()
 
 all_list = open("/home/belkiss/Desktop/current.shtml", 'r')
@@ -158,6 +203,5 @@ for element in os.listdir("/home/belkiss/download/Series"):
 					print "#######not found" + child_element
 model_found_shows.sort(0)
 kepshow.ui.found_tv_shows.setModel(model_found_shows)
-
 kepshow.show()
 sys.exit(app.exec_())
